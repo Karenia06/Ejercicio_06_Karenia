cac76bd27d4fc87bcfa1d145403cd02be167332e
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 21:25:51 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_06_Karenia/Forma.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_06_Karenia
+{
+    class Forma
+    {
+        string nom;
+        double area;
+        double perimetro;
+        public Forma() { }
+        public Forma(string nom, double area, double perimetro)
+        {
+            this.nom = nom;
+            this.area = area;
+            this.perimetro = perimetro;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public double Area { get => area; set => area = value; }
+        public double Perimetro { get => perimetro; set => perimetro = value; }
+
+        public virtual double CalArea()
+        {
+            return this.Area;
+        }
+        public virtual double CalPerimetro()
+        {
+            return this.Perimetro;
+        }
+        public override string ToString()
+        {
+            return "\nNombre: " + this.Nom + " \nArea: " + CalArea() + "\nPerimetro: " + CalPerimetro();
+        }
+    }
+    class Rectangulo : Forma
+    {
+        double ld1;
+        double ld2;
+
+        public Rectangulo(string nom, double ld1, double ld2)
+        {
+            this.ld1 = ld1;
+            this.ld2 = ld2;
+            this.Nom = nom;
+        }
+
+        public double Ld1 { get => ld1; set => ld1 = value; }
+        public double Ld2 { get => ld2; set => ld2 = value; }
+        public override double CalArea()
+        {
+            this.Area = this.ld1 * this.ld2;
+            return base.CalArea();
+        }
+        public override double CalPerimetro()
+        {
+            this.Perimetro = 2 * (this.ld1 + this.ld2);
+            return base.CalPerimetro();
+        }
+    }
+    class Circulo : Forma
+    {
+        double radio;
+        public Circulo(string nom, double radio)
+        {
+            this.Radio = radio;
+            this.Nom = nom;
+        }
+        public double Radio { get => radio; set => radio = value; }
+        public override double CalArea()
+        {
+            this.Area = Math.PI * this.Radio;
+            return base.CalArea();
+        }
+        public override double CalPerimetro()
+        {
+            this.Perimetro = 2 * Math.PI * this.Radio;
+            return base.CalPerimetro();
+        }
+    }
+}
---
+++ Ejercicio_06_Karenia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_06_Karenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rec1 = new Rectangulo("Rectangulo 1", 20, 30);
+            Circulo cir1 = new Circulo("Circulo 1", 30);
+
+            Console.WriteLine(rec1.ToString());
+            Console.WriteLine(cir1.ToString());
+            Console.ReadKey();
+        }
+    }
+}
